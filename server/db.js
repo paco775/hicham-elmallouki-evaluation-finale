956ed3b4a56e2f79f3719c2a67b68cf570cacf07
Hicham <hichamelmallouki123@gmail.com>
2026-01-21 16:13:09 -0500

    Fix MySQL env variables for Render

--- server/db.js
+++ server/db.js
@@ -1,15 +1,19 @@
 import mysql from "mysql2";
-import dotenv from "dotenv";
-dotenv.config();
 
-export const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
-  if (err) console.error("❌ Erreur connexion MySQL :", err);
-  else console.log("✅ Connecté à MySQL");
+  if (err) {
+    console.error("❌ Erreur connexion MySQL :", err);
+    return;
+  }
+  console.log("✅ Connecté à MySQL");
 });
+
+export default db;
